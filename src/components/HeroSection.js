056acc152c952c527f0483057dbe0df8da44fd15
terance-edmonds <teranceedmonds20@gmail.com>
2020-11-06 13:07:32 +0530

    2020-11-06

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -25,7 +25,7 @@ function HeroSection({
 
                                 <div className="top-line"> {topLine} </div>
                                 <h1 className={lightText ? 'heading' : 'heading dark'}> {headline} </h1>
-                                <p className={lightTextDesc ? 'home__hero-subtitle' : 'home__hero-section dark'}> {description} </p>
+                                <p className={lightTextDesc ? 'home__hero-subtitle' : 'home__hero-subtitle dark'}> {description} </p>
                                 <Link to='/sign-up'> 
                                     <Button buttonSize='btn--wide' buttonColor='blue'> {buttonLabel} </Button>
                                 </Link>
